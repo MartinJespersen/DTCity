7f583736fd1f5bebdbd173af04bf973fc6c909e3
Martin Gildberg Jespersen <au593618@uni.au.dk>
2025-05-20 09:40:15 +0200

    glm added

--- ui/ui.hpp
+++ ui/ui.hpp
@@ -6,6 +6,12 @@
 #include FT_FREETYPE_H
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
 #include <string>
 #include <stdio.h>
 // profiler
