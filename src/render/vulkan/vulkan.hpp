2cb34751d599aef05d0bddf057186982b76e66c4
Martin Jespersen <martinx1995x@hotmail.com>
2025-11-18 15:19:45 +0100

    cmake version change for clang-tidy config file

--- src/render/vulkan/vulkan.hpp
+++ src/render/vulkan/vulkan.hpp
@@ -278,7 +278,7 @@ VK_ImageFromKtx2file(VkCommandBuffer cmd, VkImage image, VK_BufferAllocation sta
 // ~mgj: Vulkan Lifetime
 static void
 VK_CtxSet(VK_Context* vk_ctx);
-VK_Context*
+g_internal VK_Context*
 VK_CtxGet();
 
 // ~mgj: Building
